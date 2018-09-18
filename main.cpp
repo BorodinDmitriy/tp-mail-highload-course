7f1b66ef81e3fa17e89431aa74a32470e6b06e7a
Borodin Dmitriy <demansen@yandex.ru>
2018-09-19 00:32:58 +0300

    simple server using boost::asio to send response to a client

--- main.cpp
+++ main.cpp
@@ -1,5 +1,43 @@
-#include <stdio.h>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+std::string makeTestResponseString() 
+{
+    std::time_t current_time = std::time(0);
+    return std::ctime(&current_time);
+}
 
 int main(void) {
-	return 0;
+    try
+    {
+        // At least one io_service is needed in boost::asio program to initiate asynchronous operations
+        boost::asio::io_service io_service;
+
+        // Acceptor is needed for listening to new connections
+        tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(),8000));
+
+        for (;;)
+        {
+            // Create a socket
+            tcp::socket socket(io_service);
+            // Make acceptor wait and listen for new connections
+            acceptor.accept(socket);
+            // Make response string to send to a client
+            std::string response = makeTestResponseString();
+
+            // Error for writing in buffer
+            boost::system::error_code error_code;
+            // Write a response to client
+            boost::asio::write(socket, boost::asio::buffer(response), error_code);
+        }
+    }
+    catch (std::exception& e)
+    {
+         std::cerr << e.what() << std::endl;
+    }
+    return 0;
 }
